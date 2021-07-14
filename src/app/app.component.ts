2b426be1a132a96c7366bff116e185a46e498869
Hollman Rojas <66705358+hollrojm@users.noreply.github.com>
2021-07-14 15:10:23 -0500

    design-page-home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'home', url: '/pages/home', icon: 'warning' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
