8c4e7ecd8a1acb7d32bd54bdd19f39db2fd10820
David Galeano <davidgaleano2611@gmail.com>
2021-08-25 16:51:56 -0500

    home changes

--- src/app/pages/hospitals/hospitals.page.ts
+++ src/app/pages/hospitals/hospitals.page.ts
@@ -15,7 +15,7 @@ export class HospitalsPage implements OnInit {
 
   form:FormGroup;
   modalReady = false;
-  constructor(private modalCtrl: ModalController, public toastController: ToastController, public alertController: AlertController, private hospitalsService: HospitalService, private loadingCtrl: LoadingController) { }
+  constructor(private modalCtrl: ModalController, public toastController: ToastController, public alertCtrl: AlertController, private hospitalsService: HospitalService, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -31,24 +31,36 @@ export class HospitalsPage implements OnInit {
       this.modalReady = true;
     }, 2300)
   }
-  async presentToast1(){
-    const toast = await this.toastController.create({
-        message: 'La institucion fue creada correctamente',
-        duration:2000,
-        animated: false,
-        position: 'bottom'
-    });
-    toast.present()
-  }
-  async presentAlert1(){
-    const alert = await this.alertController.create({
+  // async presentToast1(){
+  //   const toast = await this.toastController.create({
+  //       message: 'La institucion fue creada correctamente',
+  //       duration:2000,
+  //       animated: false,
+  //       position: 'bottom'
+  //   });
+  //   toast.present()
+  // }
+  async showAlert(){
+    await this.alertCtrl.create({
       header: "Crear Hospital",
       message: 'Se ha guardado el hospital correctamente',
-      buttons: ["OK"],
-    });
-    await alert.present()
-    let result = await alert.onDidDismiss();
-    console.log(result);
+      inputs: [
+        { type: 'text', name: 'promo', placeholder: "Test Code"}
+      ],
+      buttons: [
+        {
+          text: "Apply", handler:(res)=>{
+            console.log(res.promo);
+
+          }
+        },
+        {
+          text: "Cancel"
+        }
+      ]
+      
+    }).then(res => res.present);
+    
   }
   async submitHospital(){
     const loading = await this.loadingCtrl.create({message:'Guardando...'})
